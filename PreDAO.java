401e457a7b09717d56eb4cf043153d43b1642c8a
stefano1985 <neno1985@hotmail.it>
2016-12-14 11:43:05 +0100

    Add files via upload

---
+++ PreDAO.java
@@ -0,0 +1,46 @@
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+public class PreDAO {
+     private static PreDAO dao = null;
+     private static Connection conn = null;
+     
+     private PreDAO(){
+         InitialContext initContext =null;
+         
+         try{
+         initContext = new InitialContext();
+         
+         Context envContext = (Context)initContext.lookup("java:/comp/env");
+
+         DataSource ds = (DataSource)envContext.lookup("jdbc/miodb");
+
+         conn=ds.getConnection();
+         }
+         catch (NamingException ex) {
+             ex.printStackTrace();
+         }
+         catch(SQLException e){
+             e.printStackTrace();
+         } 
+     }
+     
+     public static PreDAO getPreDAO(){
+         if(dao==null){
+             dao=new PreDAO();
+         }
+         return dao;
+     }
+     public static Connection getConnection(){
+         if(conn==null){
+             dao=new PreDAO();
+         }
+         return conn;
+     }
+}
